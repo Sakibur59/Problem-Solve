cacd5f753e723e0a77083907ca0168e19c08fc12
Sakibur59 <sakiburrahman5978@gmail.com>
2026-02-05 11:58:38 +0600

    Calculating total salary using experience and increment

---
+++ salary.js
@@ -0,0 +1,20 @@
+const employees = [
+  { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+  { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+  { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+  { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+];
+
+
+function calculateTotalSalary(employees){
+    let totalSalary = 0;
+    for (const element of employees) {
+        let Salary = (element.experience * element.increment) + element.starting;
+       totalSalary = totalSalary + Salary;
+    }
+
+   return totalSalary;
+}
+
+let output = calculateTotalSalary(employees);
+console.log("total salary has to be provided by the company in a month: ",output);
